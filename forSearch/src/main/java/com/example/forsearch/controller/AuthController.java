d447ccf9df6b798144d57d835509cf247b72cb2f
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2023-01-02 23:15:16 +0500

    AuthController is updated

--- forSearch/src/main/java/com/example/forsearch/controller/AuthController.java
+++ forSearch/src/main/java/com/example/forsearch/controller/AuthController.java
@@ -6,13 +6,17 @@ import com.example.forsearch.config.JwtProvider;
 import com.example.forsearch.entity.forSecurity.User;
 import com.example.forsearch.payload.forSecurity.LoginDTO;
 import com.example.forsearch.payload.forSecurity.RegisterDTO;
+import com.example.forsearch.repository.forSecurity.UserRepository;
 import com.example.forsearch.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -27,6 +31,14 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public HttpEntity<?> getAll(){
+        List<User> userList = userRepository.findAll();
+        return ResponseEntity.ok(userList);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO dto) {
@@ -50,4 +62,12 @@ public class AuthController {
         var response = authService.register(registerDTO);
         return ResponseEntity.status(response.isSuccess() ? 200 : 401).body(response);
     }
+
+    @PreAuthorize(value = "hasAnyRole('SUPER_ADMIN')")
+    @PutMapping("/editRole/{id}")
+    public ResponseEntity<?> changeRole(@PathVariable Integer id, @CurrentUser User user) {
+        var response = authService.editRole(id, user);
+        return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.NOT_FOUND).body(response);
+    }
+
 }
