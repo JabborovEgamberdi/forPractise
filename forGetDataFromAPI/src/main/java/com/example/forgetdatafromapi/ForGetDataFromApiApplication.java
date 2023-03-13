0590e8c9e86aa22fda2f41ff850f81d30f505a3b
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2023-03-13 19:25:11 +0500

    new classes are added

---
+++ forGetDataFromAPI/src/main/java/com/example/forgetdatafromapi/ForGetDataFromApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.forgetdatafromapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGetDataFromApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForGetDataFromApiApplication.class, args);
+    }
+
+}
